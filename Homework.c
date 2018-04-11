29e6c52d6fb670a629fa98a84d1758e66094f653
khs0348 <38197154+khs0348@users.noreply.github.com>
2018-04-11 17:12:20 +0900

    Update Homework.c

--- Homework.c
+++ Homework.c
@@ -17,7 +17,21 @@ int Mul(int num1 , int num2)
 
 int main(){
  
- printf("Open Sourse HomeWork...");
- printf("사칙연산이 가능한 프로그램입니다.");
+ int input; //입력변수
+ 
+ printf("Open Sourse HomeWork...\n");
+ printf("사칙연산이 가능한 프로그램입니다.\n");
+ 
+ while(input =!5){
+  
+  printf("1.더하기\n");
+  printf("2.빼기\n");
+  printf("3.곱하기\n");
+  printf("4.나누기\n");
+  printf("5.종료\n");
+  printf("메뉴를 선택하세요\n");
+  
+ }
+ 
  
 }
